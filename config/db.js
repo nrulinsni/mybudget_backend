ea14bfc6284688b6c558fd50afe8fb444a4d3228
nrulinsni <ninsani79@gmail.com>
2025-07-07 02:22:39 +0700

    Add debug logs

--- config/db.js
+++ config/db.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+console.log("--- Reading Environment Variables from Server ---");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("---------------------------------------------");
+
 // buat instance Sequelize untuk koneksi database
 const sequelize = new Sequelize(
   process.env.DB_NAME,      // nama database dari file .env
